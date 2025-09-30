6af1a49bddc3edf6b92f15f1417b9a825b159cef
CHINESE <earth@sun.galaxy>
2025-09-30 14:32:49 +0800

    Init

--- MDM-424.sh
+++ MDM-424.sh
@@ -8,6 +8,11 @@ export PATCH_OUTPUT_DIR="${SCRIPT_DIR}/target/${BUG_NUMBER}"
 #export PATCH_TIMESTAMP=$(date +%Y%m%d)
 export PATCH_TIMESTAMP=20250917
 
+# for DevOps CI build and package
+if [[ -d "${SCRIPT_DIR}/../mdm/mdm-server" ]]; then
+  export MDM_SOURCE_PATH=$(cd "${SCRIPT_DIR}/../mdm/mdm-server" && pwd)
+fi
+
 # git show --name-only HEAD > "${SCRIPT_DIR}/bug/${BUG_NUMBER}.txt"
 # git show --name-only f3c8fe9d46ac5a1f011ce0b005034bb3aaf730c8 > "${SCRIPT_DIR}/bug/${BUG_NUMBER}.txt"
 
